3483e3633c078717411ac7ac6fa3c25754d03836
IT-GAL <fan-mag@mail.ru>
2019-11-20 16:26:43 +0300

    Fixed /passport GET

--- src/main/java/helpers/PassportHelper.kt
+++ src/main/java/helpers/PassportHelper.kt
@@ -1,12 +1,13 @@
 package helpers
 
 import model.Passport
+import model.Person
 import webservice.BaseService
 
 class PassportHelper : DatabaseHelper() {
     fun getPassport(userId: Int, seria: Int, number: Int): List<Passport> {
         val query = StringBuilder("SELECT * FROM passport ")
-        if (userId == 0)
+        if (userId != 0)
             query.append("WHERE user_id = $userId")
         else {
             if (seria != 0) {
@@ -20,7 +21,12 @@ class PassportHelper : DatabaseHelper() {
         val passports = ArrayList<Passport>()
         val rs = conn.createStatement().executeQuery(query.toString())
         while (rs.next()) {
-            val person = PersonHelper().getPerson(rs.getInt("user_id"))[0]
+            var personId: Int? = rs.getInt("user_id")
+            if (rs.wasNull()) personId = null
+            val person: Person?
+            if (personId != null)
+                person = PersonHelper().getPerson(rs.getInt("user_id"))[0]
+            else person = null
             val passport = Passport(rs.getInt("id"), person, rs.getInt("Серия"), rs.getInt("Номер"))
             passports.add(passport)
         }
--- src/main/java/webservice/WebServicePassport.kt
+++ src/main/java/webservice/WebServicePassport.kt
@@ -3,6 +3,7 @@ package webservice
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,7 +13,10 @@ class WebServicePassport : BaseService() {
     @GetMapping("/passport")
     fun passportGet(@RequestParam(defaultValue = "0") user_id: Int,
                     @RequestParam(defaultValue = "0") seria: Int,
-                    @RequestParam(defaultValue = "0") number: Int): ResponseEntity<Any> {
+                    @RequestParam(defaultValue = "0") number: Int,
+                    @RequestHeader(value = "Api-Key", defaultValue = "") apiKey: String): ResponseEntity<Any> {
+        incrementCount()
+        validateApiKey(apiKey, 5)
         val passports = dbPassport.getPassport(user_id, seria, number)
         return ResponseEntity(passports, HttpStatus.OK)
     }
