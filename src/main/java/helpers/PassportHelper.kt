09f70ac8a4d77fcf2cf9f08a723f7f2d24026d1a
IT-GAL <fan-mag@mail.ru>
2019-11-20 17:47:28 +0300

    /passport GET refactor

--- src/main/java/helpers/PassportHelper.kt
+++ src/main/java/helpers/PassportHelper.kt
@@ -6,20 +6,9 @@ import webservice.BaseService
 
 class PassportHelper : DatabaseHelper() {
     fun getPassport(userId: Int, seria: Int, number: Int): List<Passport> {
-        val query = StringBuilder("SELECT * FROM passport ")
-        if (userId != 0)
-            query.append("WHERE user_id = $userId")
-        else {
-            if (seria != 0) {
-                query.append("WHERE Серия = $seria ")
-                if (number != 0)
-                    query.append("AND Номер = $number")
-            } else
-                if (number != 0)
-                    query.append("WHERE Номер = $number")
-        }
+        val query = selectQueryBuilder(userId, seria, number)
         val passports = ArrayList<Passport>()
-        val rs = conn.createStatement().executeQuery(query.toString())
+        val rs = conn.createStatement().executeQuery(query)
         while (rs.next()) {
             var personId: Int? = rs.getInt("user_id")
             if (rs.wasNull()) personId = null
@@ -30,8 +19,43 @@ class PassportHelper : DatabaseHelper() {
             val passport = Passport(rs.getInt("id"), person, rs.getInt("Серия"), rs.getInt("Номер"))
             passports.add(passport)
         }
-        if (passports.isEmpty()) throw BaseService.Success201()
-        else return passports
+        return passports
     }
 
+    fun deletePassport(id: Int?, seria: Int?, number: Int?): Boolean {
+        val query = StringBuilder("DELETE FROM passport WHERE ")
+        if (id == null) {
+            if (seria != null) {
+                query.append("Серия = $seria")
+                if (number != null) {
+                    query.append(" AND Номер = $number")
+                }
+            } else {
+                if (number != null) {
+                    query.append(" Номер = $number")
+                }
+            }
+        } else {
+            query.append("id = $id")
+        }
+
+        return true
+    }
+
+    fun selectQueryBuilder(id: Int, seria: Int, number: Int): String {
+        if (id != 0)
+            return "SELECT * FROM passport WHERE user_id = $id"
+        if (seria != 0) {
+            if (number != 0)
+                return "SELECT * FROM passport WHERE Серия = $seria AND Номер = $number"
+            return "SELECT * FROM passport WHERE Серия = $seria"
+        }
+        if (number != 0)
+            return "SELECT * FROM passport WHERE Номер = $number"
+        return "SELECT * FROM passport"
+    }
+
+    fun deleteQueryBuilder(id: Int?, seria: Int?, number: Int?): String {
+        return ""
+    }
 }
--- src/main/java/webservice/WebServicePassport.kt
+++ src/main/java/webservice/WebServicePassport.kt
@@ -1,11 +1,9 @@
 package webservice
 
+import com.jayway.jsonpath.JsonPath
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class WebServicePassport : BaseService() {
@@ -19,6 +17,20 @@ class WebServicePassport : BaseService() {
         validateApiKey(apiKey, 5)
         val passports = dbPassport.getPassport(user_id, seria, number)
         return ResponseEntity(passports, HttpStatus.OK)
+    }
+
+    @DeleteMapping("/passport")
+    fun passportDelete(@RequestHeader(value = "Api-Key", defaultValue = "") apiKey: String,
+                       @RequestHeader(value = "Content-Type", defaultValue = "") contentType: String,
+                       @RequestBody(required = true) body: String): ResponseEntity<Any> {
+        incrementCount()
+        validateApiKey(apiKey, 15)
+        val id: Int? = JsonPath.parse(body).read("$['id']")
+        val seria: Int? = JsonPath.parse(body).read("$['seria']")
+        val number: Int? = JsonPath.parse(body).read("$['number']")
+        val isPassportDeleted = dbPassport.deletePassport(id, seria, number)
+        val status: HttpStatus = if (isPassportDeleted) HttpStatus.NO_CONTENT else HttpStatus.UNPROCESSABLE_ENTITY
+        return ResponseEntity(isPassportDeleted, status)
     }
 
 }
