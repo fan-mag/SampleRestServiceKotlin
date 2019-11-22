02f39cc04560023f536c287ba1eeb4198f1f906d
IT-GAL <fan-mag@mail.ru>
2019-11-22 11:05:12 +0300

    POST /login API key validation fixed

--- src/main/java/webservice/WebServiceLogin.kt
+++ src/main/java/webservice/WebServiceLogin.kt
@@ -20,9 +20,11 @@ open class WebServiceLogin : BaseService() {
 
     @PostMapping("/login")
     fun postApiKey(@RequestBody(required = true) body: String,
-                   @RequestHeader(value = "Content-Type", defaultValue = "") contentType: String): ResponseEntity<Any> {
+                   @RequestHeader(value = "Content-Type", defaultValue = "") contentType: String,
+                   @RequestHeader(value = "Api-Key", required = false) apiKey: String?): ResponseEntity<Any> {
         incrementCount()
         validateHeaders(contentType)
+        validateApiKey(apiKey, 5)
         val login: String = parseValidStringFromJson(body, "$['login']")
         val password: String = parseValidStringFromJson(body, "$['password']")
         return ResponseEntity(ApiKey(dbCredentials.generateApiKey(login, password)), HttpStatus.ACCEPTED)
